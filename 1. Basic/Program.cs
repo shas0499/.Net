6969cc4b6a4cd047b95206804c332486edd5c637
Shaswata <shaswata.barua0499@outlook.com>
2026-09-13 18:21:06 +0530

    Add files via upload

---
+++ 1. Basic/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Single Line Comment
+
+/// <summary>
+/// Documentation Comment
+/// </summary>
+/// 
+
+string name;
+name = "John Doe";
+Console.WriteLine(name);
+
+// User Input and Data Type String
+Console.Write("Enter number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(num1);
+
+// Reference Type
+string str1 = "Hello";
+Console.WriteLine(str1);
+
+// Cal
