f99156d87825462211d9a3b342835292c0614666
Shaswata <shaswata.barua0499@outlook.com>
2026-09-14 12:12:13 +0530

    Add files via upload

---
+++ 2. OOPs/4. Inderxer/IndexerDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace IndexerDemo
+{
+    public  class Employee
+    {
+        public object[] values = new object[3];
+
+        public object this[int index]
+        {
+            get
+            {
+                return values[index];
+            }
+            set { values[index] = value; }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+
+            emp[0] = "John Doe";
+            emp[1] = 30;
+            emp[2] = "Software Engineer";
+
+            Console.WriteLine("Employee Name: " + emp[0]);
+            Console.WriteLine("Employee Age: " + emp[1]);
+            Console.WriteLine("Employee Designation: " + emp[2]);
+        }
+    }
+}
