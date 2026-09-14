4efc76f9366c0c1ddd494c666f606251ab253851
Shaswata <shaswata.barua0499@outlook.com>
2026-09-14 12:16:09 +0530

    Add files via upload

---
+++ 2. OOPs/4. Inderxer/Demo2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Demo2
+{
+    public class Student
+    {
+        public object[] std = new object[5];
+
+        public object this[int index]
+        {
+            get
+            {
+                return std[index];
+            }
+            set
+            {
+                std[index] = value;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+
+            student[0] = "John Doe";
+            student[1] = 20;
+            student[2] = "Computer Science";
+            student[3] = 3.8;
+            student[4] = true;
+            student[5] = false;
+
+            Console.WriteLine("Student Name: " + student[0]);
+            Console.WriteLine(student[1]);
+        }
+    }
+}
