d3990e202345801fa83e4fa3b0434704ccc1cd7a
Shaswata <shaswata.barua0499@outlook.com>
2026-09-14 12:17:24 +0530

    Add files via upload

---
+++ 2. OOPs/4. Inderxer/Prac2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Prac2
+{
+    public class Veicle
+    {
+        public object[] info = new object[3];
+
+        public object this[object index]
+        {
+            get { return info[(int)index]; }
+            set { info[(int)index] = value; }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Veicle veicle = new Veicle();
+            veicle[0] = "Car";
+            veicle[1] = 4;
+            veicle[2] = "Red";
+
+            Console.WriteLine("Type: {0}", veicle[0]);
+            Console.WriteLine("Wheels: {0}", veicle[1]);
+            Console.WriteLine("Color: {0}", veicle[2]);
+
+        }
+    }
+}
