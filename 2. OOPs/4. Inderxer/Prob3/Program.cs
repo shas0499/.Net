79da8f4371db19f3e6635141592059e5b181286b
Shaswata <shaswata.barua0499@outlook.com>
2026-09-14 12:18:53 +0530

    Add files via upload

---
+++ 2. OOPs/4. Inderxer/Prob3/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Prob3
+{
+    internal class Program
+    {
+        public object[] Employee = new object[3];
+
+        public object this[int index]
+        {
+            get
+            {
+                return Employee[index];
+            }
+            set
+            {
+                Employee[index] = value;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program[0] = "John Doe";
+            program[1] = "Jane Smith";
+            program[2] = "Bob Johnson";
+
+            Console.WriteLine("Names:"+program[0]+", "+program[1]+", "+program[2]);
+        }
+    }
+}
