f99a5c80ec418b3c39c1d21f63d7f81c48b18063
Shaswata <shaswata.barua0499@outlook.com>
2026-09-14 12:14:52 +0530

    Add files via upload

---
+++ 2. OOPs/4. Inderxer/Prac1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Prac1
+{
+    public class Student
+    {
+        public object[] info = new object[3];
+
+        public object this[int index]
+        {
+            get { return info[index]; }
+            set { info[index] = value; }
+        }
+    }
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+
+            student[0] = "John Doe";
+            student[1] = 20;
+            student[2] = "Computer Science";
+
+            Console.WriteLine($"Name: {student[0]}");
+            Console.WriteLine("Age: " + student[1]);
+            Console.WriteLine("Department: " + student[2]);
+        }
+    }
+}
