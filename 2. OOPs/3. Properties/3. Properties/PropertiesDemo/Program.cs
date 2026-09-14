8ba17932371dcf173e40124586f5438be5abe1c3
Shaswata <shaswata.barua0499@outlook.com>
2026-09-14 12:07:35 +0530

    Add files via upload

---
+++ 2. OOPs/3. Properties/3. Properties/PropertiesDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PropertiesDemo
+{
+    public class Circle
+    {
+        double radius = 32.34;
+        public double getRadius()
+        {
+            return radius;
+        }
+        public double setRadius(double radius)
+        {
+            this.radius = radius; // this keyword is used to refer to the current instance of the class
+            return radius;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle();
+            circle.setRadius(10.5);
+            Console.WriteLine("Radius: " + circle.getRadius());
+        }
+    }
+}
