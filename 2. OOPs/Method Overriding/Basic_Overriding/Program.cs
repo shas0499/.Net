fefd45e869aaa0eb6015c2c8e9c2ffa4d624a48c
Shaswata <shaswata.barua0499@outlook.com>
2026-09-19 13:02:41 +0530

    Add files via upload

---
+++ 2. OOPs/Method Overriding/Basic_Overriding/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Basic_Overriding
+{
+    internal class Program
+    {
+        class A
+        {
+            public void Method1()
+            {
+                Console.WriteLine("Class A --- Method 1");
+            }
+            public virtual void Method2()
+            {
+                Console.WriteLine("Class A ----- Method 2");
+            }
+        }
+        class B : A
+        {
+            public override void Method2()
+            {
+                Console.WriteLine("Override Class B");
+            }
+            static void Main(string[] args)
+            {
+                B b = new B();
+                b.Method1();
+                b.Method2();
+            }
+        }
+        
+    }
+}
---
+++ 2. OOPs/Method Overriding/MethodHiding/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace MethodHiding
+{
+    internal class Program
+    {
+        class A
+        {
+            public void Method1()
+            {
+                Console.WriteLine("Class A -- Method 1");
+            }
+            public void Method2()
+            {
+                Console.WriteLine("Class A --- Method 2");
+            }
+
+        }
+        class B : A
+        {
+            public new void Method2()
+            {
+                Console.WriteLine("Class B Override");
+            }
+        }
+        static void Main(string[] args)
+        {
+            B b = new B();
+            b.Method2();
+            b.Method1();
+        }
+    }
+}
