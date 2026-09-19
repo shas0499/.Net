cb7da393d6fbd8451b4e789118250c8d47177cf4
Shaswata <shaswata.barua0499@outlook.com>
2026-09-19 12:59:26 +0530

    Add files via upload

---
+++ 2. OOPs/Interface/InterfaceP1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace InterfaceP1
+{
+    internal class Program
+    {
+        interface IShape
+        {
+            double Area();
+        }
+        class Circle : IShape
+        {
+            private double radius;
+            public Circle(double radius)
+            {
+                this.radius = radius;
+            }
+            public double Area()
+            {
+                return Math.PI * radius * radius;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(5);
+            double area = circle.Area();
+            Console.WriteLine($"Area of the circle: {area}");
+        }
+    }
+}
---
+++ 2. OOPs/Interface/Interface_Polymorphism/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Interface_Polymorphism
+{
+    internal class Program
+    {
+        // Interface
+        public interface IAnimal
+        {
+            void MakeSound();
+            void Eat();
+        }
+
+        public class Cat : IAnimal
+        {
+            public void MakeSound()
+            {
+                Console.WriteLine("Meow");
+            }
+            public void Eat()
+            {
+                Console.WriteLine("The cat is eating.");
+            }
+        }
+        static void Main(string[] args)
+        {
+            IAnimal animal = new Cat();
+            animal.MakeSound();
+            animal.Eat();
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
