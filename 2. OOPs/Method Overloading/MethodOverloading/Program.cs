0357300ecf2e26e635f0bb77f4b765e1d6fd6ccb
Shaswata <shaswata.barua0499@outlook.com>
2026-09-19 13:00:34 +0530

    Add files via upload

---
+++ 2. OOPs/Method Overloading/MethodOverloading/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace MethodOverloading
+{
+    internal class Program
+    {
+        public void Method()
+        {
+            Console.WriteLine("Method 1 Call..");
+        }
+        public void Method(string name)
+        {
+            Console.WriteLine($"{name}");
+        }
+        public void Method(int age, string city)
+        {
+            Console.WriteLine($"{age} {city}");
+        }
+        
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Method();
+            p.Method("Shaswata");
+            p.Method(27, "Kolkata");
+        }
+    }
+}
